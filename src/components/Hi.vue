f6bc5558ef73e141313059fa9a1417cdfea95056
1463353697 <1463353697@qq.com>
2018-11-18 20:23:32 +0800

    first commit

---
+++ src/components/Hi.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Hi',
+  data () {
+    return {
+      msg: 'this is Hi page'
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
